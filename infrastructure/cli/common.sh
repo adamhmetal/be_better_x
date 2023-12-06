682ce60c09218eae13fb79e292f4696b99581be0
Adam Puza <adam.puza@gmail.com>
2023-12-06 23:18:31 +0100

    wip

---
+++ infrastructure/cli/common.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+COMMIT_ID=$(git log --format="%h" -n 1)
+IMAGE_TAG="$REGISTRY_URL:$COMMIT_ID"
+
+function build_image() {
+    echo "Image: $IMAGE_TAG"
+    docker build -f infrastructure/docker/Dockerfile -t "$IMAGE_TAG" .
+}
+
+function push_image() {
+    docker build -f infrastructure/docker/Dockerfile -t "$IMAGE_TAG" .
+}
+
+# $1: 0=run forever with inotifywait, 1=run once and exit
+function run_unit_tests() {
+    echo "Running unit tests"
+    COMMIT_ID=$COMMIT_ID docker-compose -f infrastructure/docker/docker-compose.yml run -e LIVE_RELOAD=$1 --rm --service-ports --entrypoint /vol/app/tests/cli/run_unit_tests.sh app
+}
+
+# $1: 0=run forever with inotifywait, 1=run once and exit
+function run_integration_tests() {
+    echo "Running integration tests"
+    COMMIT_ID=$COMMIT_ID docker-compose -f infrastructure/docker/docker-compose.yml run -e LIVE_RELOAD=$1 --rm --service-ports --entrypoint /vol/app/tests/cli/run_integration_tests.sh app
+}
---
+++ infrastructure/cli/run_tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+print_usage_and_exit() {
+    echo "Usage: $0 [-r] [-t <unit|integration>]" 1>&2
+    exit 1
+}
+
+live_reload=false
+
+while getopts "rt:" opt; do
+    case "${opt}" in
+        r)
+            live_reload=true
+            ;;
+        t)
+            type=${OPTARG}
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+        *)
+            print_usage_and_exit
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ "${live_reload}" = true ] && [ -z "${type}" ]; then
+    print_usage_and_exit
+fi
+
+source infrastructure/cli/common.sh
+
+build_image
+
+if [ "${live_reload}" = true ]; then
+    if [ "${type}" = "unit" ]; then
+        run_unit_tests true
+    elif [ "${type}" = "integration" ]; then
+        run_integration_tests true
+    else
+        echo "Invalid test type: ${type}" >&2
+        print_usage_and_exit
+    fi
+else
+    run_unit_tests false && run_integration_tests false
+fi

2edd317d1202f9ffeeda1ef8dbac9589aaeafabd
Adam Puza <adam.puza@gmail.com>
2023-12-07 15:22:20 +0100

    wip

--- src/be_better/x.py
+++ src/be_better/x.py
@@ -1,3 +1,35 @@
+"""
+Main
+"""
 
-def x():
+
+def x() -> int:
+    """
+    Docs
+    """
     return 1
+
+
+class X:
+    """
+    Docs
+    """
+
+    def __init__(self):
+        pass
+
+    @property
+    def x(self) -> int:
+        """
+
+        :return:
+        """
+        return 1
+
+    @staticmethod
+    def v() -> int:
+        """
+
+        :return:
+        """
+        return 4
